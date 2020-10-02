25f83c343f9a6ee0a1e648af865b22c5c50200d5
Abduraghmaan Gabriels <abduraghmaangabriels2@gmail.com>
2020-10-02 14:09:23 +0200

    created a template for a nodejs server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+
+const serverport = process.env.SERVER_PORT || 4000;
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_URI } = process.env;
+// const DB_USERPASS = DB_PASSWORD && DB_USER ? `${DB_USER}:${DB_PASSWORD}@` : "";
+
+const startServer = async () => {
+	app.listen(serverport, () => {
+		console.log("Listening on port :", serverport);
+	});
+};
+
+const accessAPI = async () => {
+	app.use("/api", require("./routes/api"));
+
+	app.use((err, req, res, next) => {
+		res.status(422).send({ error: err._message });
+	});
+};
+
+startServer();
+accessAPI();
