627be328ed9bccff9fe93de62627a628fc03e741
Abduraghmaan Gabriels <abduraghmaangabriels2@gmail.com>
2020-10-02 15:06:18 +0200

    added dotenv

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 app.use(bodyParser.json());
 
