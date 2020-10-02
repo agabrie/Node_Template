a803951de9d946b5b777d84a615481bdd2d1b303
Abduraghmaan Gabriels <abduraghmaangabriels2@gmail.com>
2020-10-02 14:17:10 +0200

    added support for mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 
 const serverport = process.env.SERVER_PORT || 4000;
 const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, DB_URI } = process.env;
-// const DB_USERPASS = DB_PASSWORD && DB_USER ? `${DB_USER}:${DB_PASSWORD}@` : "";
+const DB_USERPASS = DB_PASSWORD && DB_USER ? `${DB_USER}:${DB_PASSWORD}@` : "";
 
 const startServer = async () => {
 	app.listen(serverport, () => {
